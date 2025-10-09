386c3bdd66c55074ad49b03ee31a34a6d1a38683
demitomo570811-debug <demitomo570811@gmail.com>
2025-10-09 16:50:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "PubMed connector API is running on Koyeb!"
+    return "PubMed connector API is running!"
 
 @app.get("/search")
 def search():
-    q = request.args.get("query", "")
-    return jsonify({"query": q, "hits": []})
+    query = request.args.get("query", "")
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+    params = {
+        "db": "pubmed",
+        "term": query,
+        "retmode": "json",
+        "retmax": 5
+    }
+    response = requests.get(url, params=params)
+    return jsonify(response.json())
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
